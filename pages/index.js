a9266a35bb05ab5b093e3496c2eebba980574e07
cswithen <cody.swithenbank@gmail.com>
2021-12-06 09:19:09 -0800

    fix(root-index): added Link component
    added Link component from nextjs to route to a component instead of a website
    
    closes #12

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -17,13 +18,12 @@ export default function Home() {
         <p className={styles.description}>Check out the job board below!</p>
 
         <div className={styles.grid}>
-          <a
-            href="https://west-march.vercel.app/campaigns"
-            className={styles.card}
-          >
-            <h2>Job Board &rarr;</h2>
-            <p>Explore or Create Tasks for the group</p>
-          </a>
+          <Link href="/campaigns">
+            <a className={styles.card}>
+              <h2>Job Board &rarr;</h2>
+              <p>Explore or Create Tasks for the group</p>
+            </a>
+          </Link>
         </div>
       </main>
     </div>
