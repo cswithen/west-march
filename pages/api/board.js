8571e16dee7fd100e998b6b5160df08d726561d2
cswithen <cody.swithenbank@gmail.com>
2021-12-10 08:21:04 -0800

    refactor(board.js): update call syntax
    instead of recalling the entire notion api call, imported the function from the notion.js lib directory to remove duplicate code
    
    closes #33

--- pages/api/board.js
+++ pages/api/board.js
@@ -1,14 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { Client } from "@notionhq/client";
-
-const notion = new Client({ auth: process.env.NOTION_API_KEY });
+import { getDatabase } from "../../lib/notion";
 
 const fetchBoard = async (req, res) => {
-  const response = await notion.databases.query({
-    database_id: process.env.NOTION_JOBBOARD_DATABASE_ID,
-  });
+  const response = await getDatabase(process.env.NOTION_JOBBOARD_DATABASE_ID);
 
-  res.status(200).json({ response });
+  res.status(200).json(response);
 };
 
 export default fetchBoard;
