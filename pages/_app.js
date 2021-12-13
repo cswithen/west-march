eef3c468f85477346de7971f7c912e93063ae027
cswithen <cody.swithenbank@gmail.com>
2021-12-13 10:52:54 -0800

    fix: fontawesome rendering
    fontAwesome rendering on different lines and different sizes
    
    fix was to disable autoadding of css in the configuration of the fontAwesome component
    
    closes #38

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
