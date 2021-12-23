e3c66e7041a3a40e8889fef85636ff51b09539dc
cswithen <cody.swithenbank@gmail.com>
2021-12-23 15:14:46 -0800

    style: available posts
    converted from three column system to two column to make significant room for details
    
    closes #53

--- components/ActivePost.js
+++ components/ActivePost.js
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
 import Link from "next/link";
-import styles from "./../styles/Home.module.css";
+import styles from "./../styles/AvailablePost.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faUser,
-  faChevronDown,
-  faTimes,
-} from "@fortawesome/free-solid-svg-icons";
+import { faUser, faMapSigns, faCoins } from "@fortawesome/free-solid-svg-icons";
 
 const ActivePost = ({ activePost, characters }) => {
   const properties = activePost.properties;
-  //state
-  const [active, setActive] = useState(false);
+  let descriptionArray = [];
+
+  if (properties.Description.rich_text[0]) {
+    descriptionArray =
+      properties.Description.rich_text[0].text.content.split("\n");
+  }
 
   // console.log(properties);
   // console.log(active);
@@ -19,10 +19,48 @@ const ActivePost = ({ activePost, characters }) => {
   return (
     <div className={styles.postcard}>
       <Link href={`/campaigns/posts/${activePost.id}`} passHref>
-        <div className={styles.postcardHeader}>
-          <h2>{properties.Name.title[0].text.content}</h2>
+        <div>
+          {/* Rendering Job Title */}
+          <h2 className={styles.title}>
+            {properties.Name.title[0].text.content}
+          </h2>
           <div className={styles.postcardDetails}>
-            <p>{properties.Location.rich_text[0].text.content}</p>
+            {/* Rendering Responsible Party */}
+            {properties["Responsible Party"].rich_text[0] ? (
+              <p className={styles.responsibleParty}>
+                {properties["Responsible Party"].rich_text[0].text.content}
+              </p>
+            ) : (
+              ""
+            )}
+            {/* Rendering Description */}
+            {descriptionArray.map((paragraph, index) => {
+              return (
+                <p className={styles.descriptionText} key={index}>
+                  {paragraph}
+                </p>
+              );
+            })}
+            {/* Rendering Location */}
+            {properties.Location.rich_text[0] ? (
+              <p className={styles.locationText}>
+                {" "}
+                <FontAwesomeIcon icon={faMapSigns} />{" "}
+                {properties.Location.rich_text[0].text.content}
+              </p>
+            ) : (
+              ""
+            )}
+            {/* Rendering Reward */}
+            {properties.Reward.rich_text[0] ? (
+              <p className={styles.reward}>
+                <FontAwesomeIcon icon={faCoins} />{" "}
+                {properties.Reward.rich_text[0].text.content}
+              </p>
+            ) : (
+              ""
+            )}
+            {/* Rendering Characters */}
             <p className={styles.characterCount}>
               <FontAwesomeIcon icon={faUser} />{" "}
               {properties.Characters.relation.length}
@@ -30,28 +68,6 @@ const ActivePost = ({ activePost, characters }) => {
           </div>
         </div>
       </Link>
-      <div
-        className={`${styles.postcardFooter} ${
-          active ? styles.activeCard : null
-        }`}
-      >
-        <p className={styles.descriptionText}>
-          {properties.Description.rich_text[0].text.content}
-        </p>
-        <button
-          className={styles.toggleDescription}
-          onClick={() => setActive(!active)}
-        >
-          <FontAwesomeIcon
-            icon={faChevronDown}
-            className={styles.cardDescriptionChevronDown}
-          />
-          <FontAwesomeIcon
-            icon={faTimes}
-            className={styles.cardDescriptionTimes}
-          />
-        </button>
-      </div>
     </div>
   );
 };
