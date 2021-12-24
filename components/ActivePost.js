d3e26062365bf2dbbec91b5359fd5319423edfc0
cswithen <cody.swithenbank@gmail.com>
2021-12-23 16:49:07 -0800

    feat: character display
    displays the names of the characters involved in each job posting

--- components/ActivePost.js
+++ components/ActivePost.js
@@ -7,65 +7,98 @@ import { faUser, faMapSigns, faCoins } from "@fortawesome/free-solid-svg-icons";
 const ActivePost = ({ activePost, characters }) => {
   const properties = activePost.properties;
   let descriptionArray = [];
+  let characterArray = [];
 
   if (properties.Description.rich_text[0]) {
     descriptionArray =
       properties.Description.rich_text[0].text.content.split("\n");
   }
 
+  if (properties.Characters.relation[0]) {
+    characterArray = [];
+    properties.Characters.relation.forEach((activeCharacter) => {
+      const characterIndex = characters
+        .map(function (e) {
+          return e.id;
+        })
+        .indexOf(activeCharacter.id);
+      const newCharacter = {
+        icon: characters[characterIndex].icon,
+        name: characters[characterIndex].properties.Name.title[0].text.content,
+      };
+
+      characterArray.push(newCharacter);
+    });
+  }
+
   // console.log(properties);
-  // console.log(active);
+  // console.log(characters);
 
   return (
     <div className={styles.postcard}>
       <Link href={`/campaigns/posts/${activePost.id}`} passHref>
         <div>
-          {/* Rendering Job Title */}
-          <h2 className={styles.title}>
-            {properties.Name.title[0].text.content}
-          </h2>
-          <div className={styles.postcardDetails}>
-            {/* Rendering Responsible Party */}
-            {properties["Responsible Party"].rich_text[0] ? (
-              <p className={styles.responsibleParty}>
-                {properties["Responsible Party"].rich_text[0].text.content}
-              </p>
+          <div className={styles.postHeader}>
+            {/* Rendering Job Title */}
+            <div className={styles.postHeaderText}>
+              <h2 className={styles.title}>
+                {properties.Name.title[0].text.content}
+              </h2>
+              <div className={styles.postcardDetails}>
+                {/* Rendering Responsible Party */}
+                {properties["Responsible Party"].rich_text[0] ? (
+                  <p className={styles.responsibleParty}>
+                    {properties["Responsible Party"].rich_text[0].text.content}
+                  </p>
+                ) : (
+                  ""
+                )}
+              </div>
+            </div>
+            {activePost.icon ? (
+              <span className={styles.postIcon}>{activePost.icon.emoji}</span>
             ) : (
               ""
             )}
-            {/* Rendering Description */}
-            {descriptionArray.map((paragraph, index) => {
+          </div>
+          {/* Rendering Description */}
+          {descriptionArray.map((paragraph, index) => {
+            return (
+              <p className={styles.descriptionText} key={index}>
+                {paragraph}
+              </p>
+            );
+          })}
+          {/* Rendering Location */}
+          {properties.Location.rich_text[0] ? (
+            <p className={styles.locationText}>
+              {" "}
+              <FontAwesomeIcon icon={faMapSigns} />{" "}
+              {properties.Location.rich_text[0].text.content}
+            </p>
+          ) : (
+            ""
+          )}
+          {/* Rendering Reward */}
+          {properties.Reward.rich_text[0] ? (
+            <p className={styles.reward}>
+              <FontAwesomeIcon icon={faCoins} />{" "}
+              {properties.Reward.rich_text[0].text.content}
+            </p>
+          ) : (
+            ""
+          )}
+          {/* Rendering Characters */}
+          <p className={styles.characterCount}>
+            <FontAwesomeIcon icon={faUser} />{" "}
+            {characterArray.map((character) => {
               return (
-                <p className={styles.descriptionText} key={index}>
-                  {paragraph}
-                </p>
+                <span className={styles.characterTab} key={character.name}>
+                  {character.name}
+                </span>
               );
             })}
-            {/* Rendering Location */}
-            {properties.Location.rich_text[0] ? (
-              <p className={styles.locationText}>
-                {" "}
-                <FontAwesomeIcon icon={faMapSigns} />{" "}
-                {properties.Location.rich_text[0].text.content}
-              </p>
-            ) : (
-              ""
-            )}
-            {/* Rendering Reward */}
-            {properties.Reward.rich_text[0] ? (
-              <p className={styles.reward}>
-                <FontAwesomeIcon icon={faCoins} />{" "}
-                {properties.Reward.rich_text[0].text.content}
-              </p>
-            ) : (
-              ""
-            )}
-            {/* Rendering Characters */}
-            <p className={styles.characterCount}>
-              <FontAwesomeIcon icon={faUser} />{" "}
-              {properties.Characters.relation.length}
-            </p>
-          </div>
+          </p>
         </div>
       </Link>
     </div>
