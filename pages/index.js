236d487876bb03b6947ed098b6ec466e2bbc85d1
cswithen <cody.swithenbank@gmail.com>
2022-01-10 08:27:34 -0800

    style: home directory
    styled home navigation page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Directory.module.css";
 import Link from "next/link";
 import Layout from "../components/Layout";
 
@@ -16,13 +16,13 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>West March Campaign Assistant</h1>
 
-        <p className={styles.description}>Check out the job board below!</p>
-
         <div className={styles.grid}>
           <Link href="/campaigns">
             <a className={styles.card}>
-              <h2>Job Board &rarr;</h2>
-              <p>Explore or Create Tasks for the group</p>
+              <h2>Job Board</h2>
+              <p className={styles.description}>
+                Explore or Create Tasks for the group
+              </p>
             </a>
           </Link>
         </div>
